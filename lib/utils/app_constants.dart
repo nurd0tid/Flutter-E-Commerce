a2017197a08372dd2fd518196480a45d6c5680a1
nurd0tid <mnur29054@gmail.com>
2022-10-17 16:43:31 +0700

    route api for register

--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -2,10 +2,13 @@ class AppConstants {
   static const String APP_NAME = 'Taxnow';
   static const int APP_VERSION = 1;
 
-  static const String BASE_URL = "your url backend";
+  static const String BASE_URL = "your local";
   static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
   static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommended";
   static const String DRINKS_URI = "/api/v1/products/drinks";
+
+  // auth end point
+  static const String REGISTRATION_URI = "/api/v1/auth/register";
 
   static const String UPLOAD_URL = "/uploads/";
   static const String TOKEN = "DBtoken";
