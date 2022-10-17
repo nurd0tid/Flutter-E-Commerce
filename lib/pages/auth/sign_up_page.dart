ebde64d7e534b99db3d4d18c9b3833190ce70281
nurd0tid <mnur29054@gmail.com>
2022-10-17 14:11:31 +0700

    page design

--- lib/pages/auth/sign_up_page.dart
+++ lib/pages/auth/sign_up_page.dart
@@ -1,7 +1,10 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:taxnow_beta/utils/colors.dart';
 import 'package:taxnow_beta/utils/dimension.dart';
 import 'package:taxnow_beta/widgets/app_text_field.dart';
+import 'package:taxnow_beta/widgets/big_text.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
@@ -9,10 +12,14 @@ class SignUpPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var emailController = TextEditingController();
+    var passwordController = TextEditingController();
+    var nameController = TextEditingController();
+    var phoneController = TextEditingController();
     return Scaffold(
       backgroundColor: Colors.white,
       body: Column(
         children: [
+          // Logo
           SizedBox(
             height: Dimensions.screenHeight * 0.05,
           ),
@@ -28,11 +35,69 @@ class SignUpPage extends StatelessWidget {
               ),
             ),
           ),
+          // Email
           AppTextField(
             textController: emailController,
             hintText: "Email",
             icon: Icons.email,
-          )
+          ),
+          // Password
+          SizedBox(
+            height: Dimensions.height20,
+          ),
+          AppTextField(
+            textController: passwordController,
+            hintText: "Password",
+            icon: Icons.password_sharp,
+          ),
+          // Your Name
+          SizedBox(
+            height: Dimensions.height20,
+          ),
+          AppTextField(
+            textController: nameController,
+            hintText: "Name",
+            icon: Icons.person,
+          ),
+          // Your Phone
+          SizedBox(
+            height: Dimensions.height20,
+          ),
+          AppTextField(
+            textController: phoneController,
+            hintText: "Phone",
+            icon: Icons.phone,
+          ),
+          SizedBox(
+            height: Dimensions.height20 + Dimensions.height20,
+          ),
+          Container(
+            width: Dimensions.screenWidth / 2,
+            height: Dimensions.screenHeight / 13,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(Dimensions.radius30),
+              color: AppColors.mainColor,
+            ),
+            child: Center(
+              child: BigText(
+                text: "Sign up",
+                size: Dimensions.font20 + Dimensions.font20 / 2,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: Dimensions.height10,
+          ),
+          RichText(
+            text: TextSpan(
+                recognizer: TapGestureRecognizer()..onTap = () => Get.back(),
+                text: "Have an account already?",
+                style: TextStyle(
+                  color: Colors.grey[500],
+                  fontSize: Dimensions.font20,
+                )),
+          ),
         ],
       ),
     );
