b6ecb696485d9de8d3d9f23c2ffe8d0e63d799a9
nurd0tid <mnur29054@gmail.com>
2022-10-17 17:01:19 +0700

    change radius for design sign and sign up most better

--- lib/widgets/app_text_field.dart
+++ lib/widgets/app_text_field.dart
@@ -24,12 +24,12 @@ class AppTextField extends StatelessWidget {
       ),
       decoration: BoxDecoration(
         color: Colors.white,
-        borderRadius: BorderRadius.circular(Dimensions.radius30),
+        borderRadius: BorderRadius.circular(Dimensions.radius15),
         boxShadow: [
           BoxShadow(
-              blurRadius: 10,
-              spreadRadius: 7,
-              offset: Offset(1, 10),
+              blurRadius: 3,
+              spreadRadius: 1,
+              offset: Offset(1, 1),
               color: Colors.grey.withOpacity(0.2))
         ],
       ),
@@ -42,21 +42,21 @@ class AppTextField extends StatelessWidget {
             color: AppColors.yellowColor,
           ),
           focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(Dimensions.radius30),
+            borderRadius: BorderRadius.circular(Dimensions.radius15),
             borderSide: BorderSide(
               width: 1.0,
               color: Colors.white,
             ),
           ),
           enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(Dimensions.radius30),
+            borderRadius: BorderRadius.circular(Dimensions.radius15),
             borderSide: BorderSide(
               width: 1.0,
               color: Colors.white,
             ),
           ),
           border: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(Dimensions.radius30),
+            borderRadius: BorderRadius.circular(Dimensions.radius15),
           ),
         ),
       ),
