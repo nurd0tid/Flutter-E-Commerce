72f70159e46838e85eb9cd3795aa0ea4c2b319da
nurd0tid <mnur29054@gmail.com>
2022-10-17 13:39:53 +0700

    dynamic signup

--- lib/pages/auth/sign_up_page.dart
+++ lib/pages/auth/sign_up_page.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/material.dart';
+import 'package:taxnow_beta/utils/colors.dart';
+import 'package:taxnow_beta/utils/dimension.dart';
+import 'package:taxnow_beta/widgets/app_text_field.dart';
 
 class SignUpPage extends StatelessWidget {
   const SignUpPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    var emailController = TextEditingController();
     return Scaffold(
+      backgroundColor: Colors.white,
       body: Column(
         children: [
+          SizedBox(
+            height: Dimensions.screenHeight * 0.05,
+          ),
           Container(
-            child: CircleAvatar(
-              backgroundImage: AssetImage(
-                "assets/image/logo part 1.png",
+            height: Dimensions.screenHeight * 0.25,
+            child: Center(
+              child: CircleAvatar(
+                backgroundColor: Colors.white,
+                radius: 80,
+                backgroundImage: AssetImage(
+                  "assets/image/logo part 1.png",
+                ),
               ),
             ),
           ),
+          AppTextField(
+            textController: emailController,
+            hintText: "Email",
+            icon: Icons.email,
+          )
         ],
       ),
     );
---
+++ lib/widgets/app_text_field.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+
+import '../utils/colors.dart';
+import '../utils/dimension.dart';
+
+class AppTextField extends StatelessWidget {
+  final TextEditingController textController;
+  final String hintText;
+  final IconData icon;
+
+  const AppTextField({
+    super.key,
+    required this.textController,
+    required this.hintText,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(
+        left: Dimensions.height20,
+        right: Dimensions.height20,
+      ),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(Dimensions.radius30),
+        boxShadow: [
+          BoxShadow(
+              blurRadius: 10,
+              spreadRadius: 7,
+              offset: Offset(1, 10),
+              color: Colors.grey.withOpacity(0.2))
+        ],
+      ),
+      child: TextField(
+        controller: textController,
+        decoration: InputDecoration(
+          hintText: hintText,
+          prefixIcon: Icon(
+            Icons.email,
+            color: AppColors.yellowColor,
+          ),
+          enabledBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(Dimensions.radius30),
+            borderSide: BorderSide(
+              width: 1.0,
+              color: Colors.white,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(Dimensions.radius30),
+            borderSide: BorderSide(
+              width: 1.0,
+              color: Colors.white,
+            ),
+          ),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(Dimensions.radius30),
+          ),
+        ),
+      ),
+    );
+  }
+}
