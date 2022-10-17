dbb4835b7f078f807e2455b98bfd280eb4f156eb
nurd0tid <mnur29054@gmail.com>
2022-10-17 10:59:52 +0700

    build refresh pulldown

--- lib/pages/home/main_food_page.dart
+++ lib/pages/home/main_food_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:taxnow_beta/controller/popular_product_controller.dart';
+import 'package:taxnow_beta/controller/recommended_product_controller.dart';
 import 'package:taxnow_beta/pages/home/food_page_body.dart';
 import 'package:taxnow_beta/utils/dimension.dart';
 import 'package:taxnow_beta/widgets/big_text.dart';
@@ -14,59 +17,67 @@ class MainFoodPage extends StatefulWidget {
 }
 
 class _MainFoodPageState extends State<MainFoodPage> {
+  // Future<void> _loadResource() async {
+  //   await Get.find<PopularProductController>().getPopularProductList();
+  //   await Get.find<RecommendedProductController>().getRecommendedProductList();
+  // }
+
   @override
   Widget build(BuildContext context) {
-    // print(
-    //     "current height is +" + MediaQuery.of(context).size.height.toString());
-    return Scaffold(
-      body: Column(
-        children: [
-          // Showing header
-          Container(
-            child: Container(
-              margin: EdgeInsets.only(
-                  top: Dimensions.height45, bottom: Dimensions.height15),
-              padding: EdgeInsets.only(
-                  left: Dimensions.width20, right: Dimensions.width20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Column(
-                    children: [
-                      BigText(text: "Indonesia", color: AppColors.mainColor),
-                      Row(
-                        children: [
-                          SmallText(text: "Jakarta", color: Colors.black54),
-                          Icon(Icons.arrow_drop_down_rounded)
-                        ],
+    return RefreshIndicator(
+        child: Column(
+          children: [
+            // Showing header
+            Container(
+              child: Container(
+                margin: EdgeInsets.only(
+                    top: Dimensions.height45, bottom: Dimensions.height15),
+                padding: EdgeInsets.only(
+                    left: Dimensions.width20, right: Dimensions.width20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        BigText(text: "Indonesia", color: AppColors.mainColor),
+                        Row(
+                          children: [
+                            SmallText(text: "Jakarta", color: Colors.black54),
+                            Icon(Icons.arrow_drop_down_rounded)
+                          ],
+                        ),
+                      ],
+                    ),
+                    Container(
+                      width: Dimensions.height45,
+                      height: Dimensions.height45,
+                      child: Icon(
+                        Icons.search,
+                        color: Colors.white,
+                        size: Dimensions.iconSize24,
                       ),
-                    ],
-                  ),
-                  Container(
-                    width: Dimensions.height45,
-                    height: Dimensions.height45,
-                    child: Icon(
-                      Icons.search,
-                      color: Colors.white,
-                      size: Dimensions.iconSize24,
-                    ),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(Dimensions.radius15),
-                      color: AppColors.mainColor,
-                    ),
-                  )
-                ],
+                      decoration: BoxDecoration(
+                        borderRadius:
+                            BorderRadius.circular(Dimensions.radius15),
+                        color: AppColors.mainColor,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-          ),
-          // Showing Body Content
-          // FoodPageBody(),
-          Expanded(
-              child: SingleChildScrollView(
-            child: FoodPageBody(),
-          )),
-        ],
-      ),
-    );
+            // Showing Body Content
+            // FoodPageBody(),
+            Expanded(
+                child: SingleChildScrollView(
+              child: FoodPageBody(),
+            )),
+          ],
+        ),
+        onRefresh: () async {
+          await Get.find<PopularProductController>().getPopularProductList();
+          await Get.find<RecommendedProductController>()
+              .getRecommendedProductList();
+        });
   }
 }
--- lib/utils/app_constants.dart
+++ lib/utils/app_constants.dart
@@ -2,9 +2,10 @@ class AppConstants {
   static const String APP_NAME = 'Taxnow';
   static const int APP_VERSION = 1;
 
-  static const String BASE_URL = "your backend url";
+  static const String BASE_URL = "https://taxnowapp.com";
   static const String POPULAR_PRODUCT_URI = "/api/v1/products/popular";
   static const String RECOMMENDED_PRODUCT_URI = "/api/v1/products/recommended";
+  static const String DRINKS_URI = "/api/v1/products/drinks";
 
   static const String UPLOAD_URL = "/uploads/";
   static const String TOKEN = "DBtoken";
